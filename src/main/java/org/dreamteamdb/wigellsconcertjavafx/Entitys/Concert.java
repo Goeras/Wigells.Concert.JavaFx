36d9c50851649950ba8811ad68307e307aaf1371
Markus Göras <m.goras@hotmail.com>
2024-03-08 09:57:13 +0100

    Update Concert & Customer

--- src/main/java/org/dreamteamdb/wigellsconcertjavafx/Entitys/Concert.java
+++ src/main/java/org/dreamteamdb/wigellsconcertjavafx/Entitys/Concert.java
@@ -1,4 +1,178 @@
 package org.dreamteamdb.wigellsconcertjavafx.Entitys;
+import jakarta.persistence.*;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.HibernateException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "concert")
 public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int Id;
+
+    @Column(name = "artist_name")
+    private String artistName;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "ticket_price")
+    private double ticketPrice;
+
+    @ManyToOne
+    @Column(name = "arena_id")
+    private Arena arena;
+
+    @Column(name = "age_limit")
+    private int ageLimit;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "wc",
+    joinColumns = {@JoinColumn(name = "concert_id")},
+    inverseJoinColumns = {@JoinColumn(name = "customer_id")})
+    private List<Customer> customerList = new ArrayList<>();
+
+    // Constructor
+    public Concert() {
+    }
+
+    // CRUD-operations
+    public void createConcert(Concert concert){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        try {
+            s.persist(concert);
+            s.getTransaction().commit();
+        }
+        catch(HibernateException he){
+        he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+    public Concert getConcertById(int id){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        Concert concert = s.get(Concert.class, id);
+        s.close();
+
+        return concert;
+    }
+
+    public List<Concert> getAllConcerts(){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        List<Concert> concerts = s.createQuery("FROM Concert", Concert.class).list();
+        return concerts;
+    }
+
+    public void updateConcert(Concert concert){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        try{
+            s.merge(concert);
+            s.getTransaction().commit();
+        }
+        catch(HibernateException he){
+            he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+    public void deleteConcert(Concert concert){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        try{
+            s.remove(concert);
+        }
+        catch(HibernateException he){
+            he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public Arena getArena() {
+        return arena;
+    }
+
+    public void setArena(Arena arena) {
+        this.arena = arena;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(List<Customer> customerList) {
+        this.customerList = customerList;
+    }
+
+    public void addCustomerToList(Customer customer){
+        this.customerList.add(customer);
+    }
 }
--- src/main/java/org/dreamteamdb/wigellsconcertjavafx/Entitys/Customer.java
+++ src/main/java/org/dreamteamdb/wigellsconcertjavafx/Entitys/Customer.java
@@ -1,4 +1,179 @@
 package org.dreamteamdb.wigellsconcertjavafx.Entitys;
 
+import jakarta.persistence.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "phone_nbr")
+    private int phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @Column(name = "address_id")
+    @JoinColumn(name = "address")
+    private Address address;    // Kolla video hur denna ska vara.
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "wc",
+    joinColumns = {@JoinColumn(name = "customer_id")},
+    inverseJoinColumns = {@JoinColumn(name = "concert_id")})
+    private List<Concert> concertList = new ArrayList<>();
+
+    // Constructor
+    public Customer() {
+    }
+
+    // CRUD-operations
+    public void createConcert(Customer customer){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        try {
+            s.persist(customer);
+            s.getTransaction().commit();
+        }
+        catch(HibernateException he){
+            he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+    public Customer getCustomerById(int id){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        Customer customer = s.get(Customer.class, id);
+        s.close();
+
+        return customer;
+    }
+
+    public List<Customer> getAllCustomer(){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+
+        List<Customer> customers = s.createQuery("FROM Customer", Customer.class).list();
+        return customers;
+    }
+
+    public void updateCustomer(Customer customer){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        try{
+            s.merge(customer);
+            s.getTransaction().commit();
+        }
+        catch(HibernateException he){
+            he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+    public void deleteCustomer(Customer customer){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        try{
+            s.remove(customer);
+        }
+        catch(HibernateException he){
+            he.printStackTrace();
+        }
+        finally{
+            s.close();
+        }
+    }
+
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Concert> getConcertList() {
+        return concertList;
+    }
+
+    public void setConcertList(List<Concert> concertList) {
+        this.concertList = concertList;
+    }
+
+    public void addConcertToList(Concert concert){
+        this.concertList.add(concert);
+    }
 }
