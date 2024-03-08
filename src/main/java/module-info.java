65a14e517ce2f8b7c7c423092979d547f8c1dc25
Markus Göras <m.goras@hotmail.com>
2024-03-08 09:58:24 +0100

    Add Customer & Concert

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,29 @@
+/*
 module org.dreamteamdb.wigellsconcertjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+
+
 
 
     opens org.dreamteamdb.wigellsconcertjavafx to javafx.fxml;
     exports org.dreamteamdb.wigellsconcertjavafx;
+}*/
+
+module org.dreamteamdb.wigellsconcertjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    //requires java.persistence;
+    requires java.naming;
+
+
+    exports org.dreamteamdb.wigellsconcertjavafx;
+    opens org.dreamteamdb.wigellsconcertjavafx.Entitys;
+    exports org.dreamteamdb.wigellsconcertjavafx.Entitys;
+    opens org.dreamteamdb.wigellsconcertjavafx;
 }
--- src/main/java/org/dreamteamdb/wigellsconcertjavafx/HelloApplication.java
+++ src/main/java/org/dreamteamdb/wigellsconcertjavafx/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.dreamteamdb.wigellsconcertjavafx.Entitys.Concert;
 
 import java.io.IOException;
 
@@ -13,6 +14,12 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+
+        Concert concert = new Concert();
+        concert.setArtistName("Johnny rotten");
+        concert.createConcert(concert);
+
+
         stage.setScene(scene);
         stage.show();
     }
